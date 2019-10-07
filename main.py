bc5f601acdbffc5f330bd70fbda5b6882b93c5b3
Jason Ball <jasonericball@gmail.com>
2019-10-06 23:03:27 -0400

    Load program file from disk and submit to scheduler

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from Instruction import Instruction
 from Program import Program
+from Process import Process
+from Scheduler import Scheduler
+from PCB import PCB
+from Dispatcher import Dispatcher
+import random
+import math
+import copy
 
 
 def main():
@@ -11,7 +18,7 @@ def main():
             break
         except FileNotFoundError:
             print("File not found!")
-    program.threads = int(input("Number of threads: "))
+    program.number_of_processes = int(input("Number of processes: "))
     instructions = []
     for line in file:
         if line == 'EXE':
@@ -24,13 +31,43 @@ def main():
             program.memory = int(line.split(': ')[1])
         elif line != '\n':
             current_line = line.split()
-            instructions.append(Instruction(current_line[0], int(current_line[1])))
+            if len(current_line) == 1:
+                instructions.append(Instruction(current_line[0], 0))
+            else:
+                instructions.append(Instruction(current_line[0], int(current_line[1])))
     program.instructions = instructions
 
-    print(f'Program loaded: {program.name}({program.threads})')
+    print(f'Program loaded: {program.name}({program.number_of_processes})')
 
-    for itr in program.instructions:
-        print(f'{itr.command}: {itr.value}')
+    # for itr in program.instructions:
+    #     print(f'{itr.command}: {itr.value}')
+
+    processes = [Process(copy.deepcopy(program), pid) for pid in range(program.number_of_processes)]
+    print('Randomizing...')
+    for process in processes:
+        random.seed()
+        random.shuffle(process.program.instructions)
+        for instruction in\
+                (instruction for instruction in process.program.instructions if instruction.command == 'CALCULATE'):
+            instruction.value = random.randint(math.floor(instruction.value / 2), instruction.value * 2)
+    pcbs = [PCB(process) for process in processes]
+    for pcb in pcbs:
+        pcb.process.pcb = pcb
+
+    for pcb in pcbs:
+        print(f'------Process {pcb.process.pid}------')
+        for instruction in pcb.process.program.instructions:
+            print(f'{instruction.command}: {instruction.value}')
+        print(f'---------------------')
+    input('Press enter/return to start execution...')
+
+    dispatcher = Dispatcher()
+    scheduler = Scheduler(dispatcher)
+    scheduler.start()
+    scheduler.PCBs = pcbs
+    scheduler.PCBs_available.set()
+
+    scheduler.thread.join()
 
 
 if __name__ == '__main__':
