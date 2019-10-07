66b865cb78f59a482ec97587a9f82895f7ae43e7
Jason Ball <jasonericball@gmail.com>
2019-10-06 20:00:13 -0400

    Use dispatcher in scheduler to manage processes

--- Scheduler.py
+++ Scheduler.py
@@ -7,12 +7,21 @@ class Scheduler:
     PCBs_available = threading.Event()
     thread = None
 
+    def __init__(self, dispatcher):
+        self.dispatcher = dispatcher
+
     def run(self):
         self.PCBs_available.wait()
         for pcb in self.PCBs:
             pcb.process.start()
-            pcb.process.thread.join()
-            time.sleep(2)
+            pcb.state = 'READY'
+        while True:
+            self.PCBs[:] = [pcb for pcb in self.PCBs if not pcb.process.done]
+            for pcb in self.PCBs:
+                self.dispatcher.switch(pcb)
+                time.sleep(2)
+            if len(self.PCBs) == 0:
+                break
 
     def start(self):
         self.thread = threading.Thread(target=self.run)
