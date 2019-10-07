90436b83c6c4b26f0f4ad4293c2e63065745f6de
Jason Ball <jasonericball@gmail.com>
2019-10-06 20:00:42 -0400

    Simulate program running

--- Process.py
+++ Process.py
@@ -1,16 +1,25 @@
 import threading
+import time
 
 
 class Process:
     thread = None
     active = False
     started = False
+    done = False
+    lock = threading.Event()
 
     def task(self):
-        print(self.program.name)
+        for instruction in self.program.instructions:
+            self.lock.wait()
+            print(f'{self.pid}-> {instruction.command}: {instruction.value}')
+            time.sleep(1)
+        self.done = True
+        print(f'---{self.pid}: DONE---')
 
-    def __init__(self, program):
+    def __init__(self, program, pid):
         self.program = program
+        self.pid = pid
         self.thread = threading.Thread(target=self.task)
 
     def start(self):
