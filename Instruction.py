50214aaa14bed8157d8a6c6428f7a6663616f13e
Jason Ball <jasonericball@gmail.com>
2019-11-06 11:14:29 -0500

    Assign instructions' critical sections

--- Instruction.py
+++ Instruction.py
@@ -1,4 +1,5 @@
 class Instruction:
-    def __init__(self, command, value):
+    def __init__(self, command, value, cs=None):
         self.command = command
         self.value = value
+        self.cs = cs
