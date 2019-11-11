15a7e007b3b7023e8b3868ea2c33b7d437762db0
Jason Ball <jasonericball@gmail.com>
2019-11-10 20:13:09 -0500

    Store ready and waiting PCBs seperately

--- tables.py
+++ tables.py
@@ -1,6 +1,12 @@
-from typing import NewType, Dict
+from typing import NewType, Dict, List
 from CriticalSection import CriticalSection
+from collections import deque
+from PCB import PCB
 cs = NewType("CriticalSection", CriticalSection)
 
 critical_sections = {}
-PCBs = []
+PCBs = List[PCB]
+ready_pcbs = deque()
+waiting_pcbs = deque()
+
+memory = 4096
