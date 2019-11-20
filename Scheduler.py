df8e833fd01228bbe8432798e752b9b93166719d
Jason Ball <jasonericball@gmail.com>
2019-11-20 10:37:56 -0500

    Remove old scheduler comment

--- Scheduler.py
+++ Scheduler.py
@@ -53,35 +53,3 @@ class Scheduler:
             self.run_lock.clear()
             self.dispatcher.submit(tables.ready_pcbs.pop())
             self.run_lock.wait(1)
-
-""" class Scheduler:
-    PCBs = tables.PCBs
-    PCBs_available = threading.Event()
-    thread = None
-    wait_lock = threading.Event()
-
-    def __init__(self, dispatcher):
-        self.dispatcher = dispatcher
-
-    def run(self):
-        self.PCBs_available.wait()
-        for pcb in tables.PCBs:
-            pcb.process.start()
-            pcb.process.scheduler_lock = self.wait_lock
-            pcb.process.dispatcher = self.dispatcher
-            pcb.state = 'READY'
-        while True:
-            self.wait_lock.clear()
-            # tables.PCBs[:] = [pcb for pcb in tables.PCBs if not pcb.process.done]
-            # for pcb in tables.PCBs:
-            for pcb in [pcb for pcb in tables.PCBs if not pcb.process.done]:
-                if pcb.state == 'READY':
-                    self.dispatcher.switch(pcb)
-                    self.wait_lock.wait(1)
-            if len([pcb for pcb in tables.PCBs if not pcb.process.done]) == 0:
-                break
-
-    def start(self):
-        self.thread = threading.Thread(target=self.run)
-        self.thread.start()
- """
