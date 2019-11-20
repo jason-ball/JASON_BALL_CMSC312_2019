951d72ffc0b9b3135744e79aa98c25e4539ac169
Jason Ball <jasonericball@gmail.com>
2019-11-20 10:38:30 -0500

    Remove old Dispatcher comment

--- Dispatcher.py
+++ Dispatcher.py
@@ -54,33 +54,3 @@ class Dispatcher:
     def done_waiting(self):
         if self.scheduler is not None:
             self.scheduler.schedule()
-
-
-
-
-""" class Dispatcher:
-    active_pcb: PCB = None
-
-    def switch(self, pcb: PCB):
-        if self.active_pcb is not None:
-            self.active_pcb.process.lock.clear()
-            self.active_pcb.dispatcher = None
-            if self.active_pcb.state != 'DONE' and not self.active_pcb.process.sleeping:
-                self.active_pcb.state = 'READY'
-        self.active_pcb = pcb
-        self.active_pcb.dispatcher = self
-        self.active_pcb.process.lock.set()
-        self.active_pcb.state = 'RUN'
-
-    def clear(self):
-        if self.active_pcb is not None:
-            self.active_pcb.process.lock.clear()
-            self.active_pcb = None
-
-    def block(self):
-        self.active_pcb.state = 'WAIT'
-        self.active_pcb.process.lock.clear()
-    
-    def submit(self, pcb: PCB):
-        pass
- """
